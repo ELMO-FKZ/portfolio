dea2df0d3ece276d8297aff2bce4792a5b70d659
EL MOKHTAR FKHARZ <fkharz.el.mokhtar@gmail.com>
2023-03-07 13:43:50 +0100

    Add Javascript code of PORTFOLIO section

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let prevScrollpos = window.pageYOffset;
 
 // Event listeners
 
-    //-- Get data when the DOM get loaded 
+    //-- Get dark mode data when the DOM get loaded 
 document.addEventListener("DOMContentLoaded", getDarkMode);
     //-- Click on toggle mode checkbox
 darkToggle.addEventListener('change', changeMode);
@@ -36,6 +36,8 @@ switchItems.forEach((switchItem) => {
 switchItems.forEach((switchItem) => {
     switchItem.addEventListener("keypress", filterOnEnter);
 });
+    //-- Validate the contact form
+document.addEventListener("DOMContentLoaded", validateContactForm);
 
 // Functions
 
@@ -112,9 +114,130 @@ function manageBoxes() {
     });
 }
 
-
 function filterOnEnter(e) {
     if (e.key == "Enter") {
         e.target.click();
     }
 }
+
+function validateContactForm() {
+    const nameForm = document.querySelector("#name");
+    const emailForm = document.querySelector("#email");
+    const messageForm = document.querySelector("#message");
+    const submitButton = document.querySelector("#submit");
+
+    let nameIsValid = false;
+    let emailIsValid = false;
+    let messageIsValid = false;
+
+    // Check if one of the inputs is empty
+    if (!nameForm || !emailForm || !messageForm || !submitButton) {
+        return;
+    }
+
+    // Typing on the name input
+    nameForm.addEventListener("input", e => {
+        const nameFormValue = e.target.value;
+        if (validateName(nameFormValue)) {
+            nameIsValid = true;
+            applyValidStyle(nameForm);
+        }
+        else {
+            nameIsValid = false;
+            applyInvalidStyles(nameForm);
+        }
+        updateSubmitButton();
+    });
+
+    // Typing on the email input
+    emailForm.addEventListener("input", e => {
+        const emailFormValue = e.target.value;
+        if (validateEmail(emailFormValue)) {
+            emailIsValid = true;
+            applyValidStyle(emailForm);
+        }
+        else {
+            emailIsValid = false;
+            applyInvalidStyles(emailForm);
+        }
+        updateSubmitButton();
+    });
+
+    // Typing on message input
+    messageForm.addEventListener("input", (event) => {
+        const messageFormValue = event.target.value;
+        if (validateMessage(messageFormValue)) {
+            messageIsValid = true;
+            applyValidStyle(messageForm);
+        }
+        else {
+            messageIsValid = false;
+            applyInvalidStyles(messageForm);
+        }
+        updateSubmitButton();
+    });
+    
+    // Function to validate the name input
+    const validateName = (value) => {
+        const nameValidation = /^[a-zA-Z\u00C0-\u024F\u0027\u002E\u002D\u1E00-\u1EFF]+( [a-zA-Z\u00C0-\u024F\u0027\u002E\u002D\u1E00-\u1EFF\s]+)+$/;
+        if (value.match(nameValidation)) {
+            return true;
+        }
+        return false;
+    };
+
+    // Function to validate the email input
+    const validateEmail = (value) => {
+        const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+        if (value.match(emailValidation)) {
+            return true;
+        }
+        return false;
+    };
+
+    // Function to validate the message input
+    const validateMessage = (value) => {
+        const messageValidation = /[\S\s]+[\S]+/;
+        if (value.match(messageValidation)) {
+            return true;
+        }
+        return false;
+    };
+
+    // Function of styles when an input is not valid
+    const applyInvalidStyles = (input) => {
+        input.classList.remove("contact__form-input--valid");
+        input.classList.add("contact__form-input--error");
+    };
+
+    // Function of styles when an input is valid
+    const applyValidStyle = (input) => {
+        input.classList.remove("contact__form-input--error");
+        input.classList.add("contact__form-input--valid");
+    };
+
+    // Function of styles when the submit button is disabled
+    const disableSubmitButton = () => {
+        submitButton.disabled = true;
+        submitButton.classList.add("contact__submit--disabled");
+    };
+
+    // Function of styles when the submit button is enabled
+    const enableSubmitButton = () => {
+        submitButton.disabled = false;
+        submitButton.classList.remove("contact__submit--disabled");
+    };
+
+    // Function to apply enable & disable submit button styles
+    const updateSubmitButton = () => {
+        if (nameIsValid && emailIsValid && messageIsValid) {
+            enableSubmitButton();
+        }
+        else {
+            disableSubmitButton();
+        }
+    };
+
+    // Make sure the submit button is udated
+    updateSubmitButton();
+};
